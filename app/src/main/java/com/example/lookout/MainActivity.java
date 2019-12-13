5d317a0cd46543e97894f28ff0aa45f4c3d6ac87
Tushar Rohilla <tusharrohilla.iitr@gmail.com>
2019-12-14 00:13:05 +0530

    Map Added, app closing

--- app/src/main/java/com/example/lookout/MainActivity.java
+++ app/src/main/java/com/example/lookout/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.lookout;
 
 import android.os.Bundle;
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -9,7 +8,6 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
--- app/src/main/java/com/example/lookout/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/lookout/ui/dashboard/DashboardFragment.java
@@ -9,29 +9,58 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.lookout.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class DashboardFragment extends Fragment {
+public class DashboardFragment extends FragmentActivity implements OnMapReadyCallback {
 
-    private DashboardViewModel dashboardViewModel;
+//    private DashboardViewModel dashboardViewModel;
+    GoogleMap map;
 
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel = ViewModelProviders.of(this).get(DashboardViewModel.class);
+//    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_dashboard);
 
-        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+//        dashboardViewModel = ViewModelProviders.of(this).get(DashboardViewModel.class);
+//        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
 
 //        final TextView textView = root.findViewById(R.id.text_dashboard);
-
 //        dashboardViewModel.getText().observe(this, new Observer<String>() {
 //            @Override
 //            public void onChanged(@Nullable String s) {
 //                textView.setText(s);
 //            }
 //        });
+        SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
-        return root;
+
+//        return root;
+    }
+
+//    private FragmentManager getSupportFragmentManager() {
+//        return null;
+//    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng US = new LatLng(37.090240, -95.712891);
+        map.addMarker(new MarkerOptions().position(US).title("Unites States"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(US));
+
     }
 }
