5df662d70745023ddab1c6108e93f02e41664caf
Tushar Rohilla <tusharrohilla.iitr@gmail.com>
2019-12-16 01:47:37 +0530

    Home Page DONE

--- app/src/main/java/com/example/lookout/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/lookout/ui/home/HomeFragment.java
@@ -26,6 +26,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.util.Random;
 
 public class HomeFragment extends Fragment {
 
@@ -34,18 +35,25 @@ public class HomeFragment extends Fragment {
     private static TextView mQuoteResult;
     private static TextView mAuthorResult;
     private static final String MY_REQUEST_URL = "https://api.paperquotes.com/quotes/?tags=environment";
+    private Random rand = new Random();
+    int randomIndex = rand.nextInt(5);
 
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) { homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
-    View root = inflater.inflate(R.layout.fragment_home, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
 
         mQuoteResult = root.findViewById(R.id.quote);
+        mAuthorResult = root.findViewById(R.id.author);
 
-        HttpGetRequest requestAPI = new HttpGetRequest();
-        requestAPI.execute();
+        HttpQuoteRequest requestAPIQuote = new HttpQuoteRequest();
+        requestAPIQuote.execute();
+        HttpAuthorRequest requestAPIAuthor = new HttpAuthorRequest();
+        requestAPIAuthor.execute();
+
         return root;
     }
 
-    public static class HttpGetRequest extends AsyncTask<URL, String, String> {
+    public class HttpQuoteRequest extends AsyncTask<URL, String, String> {
 
         @Override
         protected String doInBackground(URL... urls) {
@@ -57,12 +65,12 @@ public class HomeFragment extends Fragment {
                 Log.e("errorTag", "Error with creating URL", exception);
                 return null;
             }
-            
+
             String jsonResponse = "";
             try {
                 jsonResponse = makeHttpRequest(url);
             } catch (IOException e) {
-                Log.e("errorTag","Error in request");
+                Log.e("errorTag", "Error in request");
             }
             return jsonResponse;
         }
@@ -70,8 +78,7 @@ public class HomeFragment extends Fragment {
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            mQuoteResult.setText(s);
-
+            mQuoteResult.setText("\"" + s + "\"");
         }
 
         private String makeHttpRequest(URL url) throws IOException {
@@ -86,7 +93,7 @@ public class HomeFragment extends Fragment {
                 urlConnection.connect();
 
                 inputStream = urlConnection.getInputStream();
-                jsonResponse = readQuote(inputStream);
+                jsonResponse = readInputStream(inputStream);
 
             } catch (IOException e) {
 
@@ -98,20 +105,20 @@ public class HomeFragment extends Fragment {
             return jsonResponse;
         }
 
-        private String readQuote(InputStream inputStream) throws IOException {
+        private String readInputStream(InputStream inputStream) throws IOException {
             String copyOutput = null;
-                StringBuilder output = new StringBuilder();
-                if (inputStream != null) {
-                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
-                    BufferedReader reader = new BufferedReader(inputStreamReader);
-                    String line = reader.readLine();
-                    while (line != null) {
-                        output.append(line);
-                        line = reader.readLine();
-                    }
+            StringBuilder output = new StringBuilder();
+            if (inputStream != null) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+                BufferedReader reader = new BufferedReader(inputStreamReader);
+                String line = reader.readLine();
+                while (line != null) {
+                    output.append(line);
+                    line = reader.readLine();
                 }
+            }
 
-                String finalOutput = output.toString();
+            String finalOutput = output.toString();
             JSONObject parentObject = null;
             try {
                 parentObject = new JSONObject(finalOutput);
@@ -124,15 +131,114 @@ public class HomeFragment extends Fragment {
             } catch (JSONException e) {
                 e.printStackTrace();
             }
+
+//            Random rand = new Random();                     //For generating a random quote from a JSONObject
+//            int randomIndex = rand.nextInt(5);
+
             JSONObject quoteObject = null;
             try {
-                quoteObject = parentArray.getJSONObject(0);
+                quoteObject = parentArray.getJSONObject(randomIndex);
             } catch (JSONException e) {
                 e.printStackTrace();
             }
             try {
                 String quote = quoteObject.getString("quote");
                 copyOutput = quote;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return copyOutput;
+        }
+    }
+
+    public class HttpAuthorRequest extends AsyncTask<URL, String, String> {
+
+        @Override
+        protected String doInBackground(URL... urls) {
+
+            URL url = null;
+            try {
+                url = new URL(MY_REQUEST_URL);
+            } catch (MalformedURLException exception) {
+                Log.e("errorTag", "Error with creating URL", exception);
+                return null;
+            }
+
+            String jsonResponse = "";
+            try {
+                jsonResponse = makeHttpRequest(url);
+            } catch (IOException e) {
+                Log.e("errorTag", "Error in request");
+            }
+            return jsonResponse;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            mAuthorResult.setText(s);
+        }
+
+        private String makeHttpRequest(URL url) throws IOException {
+            String jsonResponse = "";
+            HttpURLConnection urlConnection = null;
+            InputStream inputStream = null;
+            try {
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.setRequestProperty("Authorization", "Token 2e9072a007f0fcd23d80fc5537a5c174bee9ff47");
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+                urlConnection.connect();
+
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readInputStream(inputStream);
+
+            } catch (IOException e) {
+
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+            }
+            return jsonResponse;
+        }
+
+        private String readInputStream(InputStream inputStream) throws IOException {
+            String copyOutput = null;
+            StringBuilder output = new StringBuilder();
+            if (inputStream != null) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+                BufferedReader reader = new BufferedReader(inputStreamReader);
+                String line = reader.readLine();
+                while (line != null) {
+                    output.append(line);
+                    line = reader.readLine();
+                }
+            }
+
+            String finalOutput = output.toString();
+            JSONObject parentObject = null;
+            try {
+                parentObject = new JSONObject(finalOutput);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            JSONArray parentArray = null;
+            try {
+                parentArray = parentObject.getJSONArray("results");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            JSONObject authorObject = null;
+            try {
+                authorObject = parentArray.getJSONObject(randomIndex);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            try {
+                String author = authorObject.getString("author");
+                copyOutput = author;
             } catch (JSONException e) {
                 e.printStackTrace();
             }
