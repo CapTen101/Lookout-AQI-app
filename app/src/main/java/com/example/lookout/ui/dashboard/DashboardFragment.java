09a89a3fe5b0de0c8c74f8b424654f580b1d0953
Tushar Rohilla <tusharrohilla.iitr@gmail.com>
2019-12-16 17:20:06 +0530

    Dashboard work started

--- app/src/main/java/com/example/lookout/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/lookout/ui/dashboard/DashboardFragment.java
@@ -7,26 +7,72 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
+import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.lookout.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class DashboardFragment extends Fragment {
+public class DashboardFragment extends FragmentActivity implements OnMapReadyCallback {
 
     private DashboardViewModel dashboardViewModel;
+    private TextView randomText;
+    GoogleMap map;
 
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) { dashboardViewModel = ViewModelProviders.of(this).get(DashboardViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        dashboardViewModel = ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        randomText = root.findViewById(R.id.text_dashboard);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapview);
+        mapFragment.getMapAsync(this);
+        // Gets the MapView from the XML layout and creates it
+//        mapView = (MapView) root.findViewById(R.id.mapview);
+//        mapView.onCreate(savedInstanceState);
+
+        // Gets to GoogleMap from the MapView and does initialization stuff
+//        mapView.getMapAsync(this);
+
+        // Needs to call MapsInitializer before doing any CameraUpdateFactory calls
+//        MapsInitializer.initialize(this.getActivity());
+
+        // Updates the location and zoom of the MapView
+//        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(43.1, -87.9), 10);
+//        map.animateCamera(cameraUpdate);
         return root;
     }
+//
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        map.getUiSettings().setZoomControlsEnabled(true);
+        map.getUiSettings().setMyLocationButtonEnabled(false);
+        map.setMyLocationEnabled(true);
+        map.addMarker(new MarkerOptions().position(new LatLng(50, 6)));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(50, 6), 10));
+    }
+//
+//    @Override
+//    public void onResume() {
+//        mapView.onResume();
+//        super.onResume();
+//    }
+//
+//    @Override
+//    public void onDestroy() {
+//        super.onDestroy();
+//        mapView.onDestroy();
+//    }
+//
+//    @Override
+//    public void onLowMemory() {
+//        super.onLowMemory();
+//        mapView.onLowMemory();
+//    }
 }
